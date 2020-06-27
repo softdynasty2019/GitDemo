17cef73c5aed8b254c5289873fe601ef3efab003
SoftDynasty <softdynasty2019@gmail.com>
2020-06-26 20:20:48 -0400

    Code from SoftDynasty

---
+++ src/main/java/Report/ExtendReport/App.java
@@ -0,0 +1,13 @@
+package Report.ExtendReport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Report/ExtendReport/ExtentReportDemoTest.java
@@ -0,0 +1,55 @@
+package Report.ExtendReport;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportDemoTest {
+	
+	ExtentReports extent;
+
+	@BeforeTest 
+	
+	public void config() {
+		
+	//Extent Report-Generate Report //ExtentSparkReport	--Config Report
+		
+		
+		String Path=System.getProperty("user.dir")+"\\reports\\index.html";
+		
+		//It will write The report which is collected 
+		ExtentSparkReporter report=new ExtentSparkReporter(Path);
+		report.config().setReportName("Web Automation Report");
+		report.config().setDocumentTitle("Google Home Page Automation Report");
+		
+		//It will collect the Result From  the Test 
+		extent =new ExtentReports ();
+		extent.attachReporter(report);
+		extent.createTest("Test Report");
+		extent.setSystemInfo("Qa Tester","Masud Rana");
+				
+	}
+	
+	@Test
+	public void googleHomePage() {
+		
+		extent.createTest("googleHomePage");
+		System.setProperty("webdriver.chrome.driver",".\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		System.out.println("USA QA CODE");
+		driver.getTitle();
+		driver.close();
+		extent.flush();
+		
+		
+		
+	}
+	
+	
+}
